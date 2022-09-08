348288884a1a4c21caffa30f57bd531e4d641b23
Connor D <connordecamp@gmail.com>
2022-09-08 13:04:59 -0500

    Solve day 13 part one

---
+++ src/thirteen.cpp
@@ -0,0 +1,224 @@
+/* Day 13: Transparent Origami
+ *
+ */
+
+#include <bits/stdc++.h>
+
+enum class FoldType {
+    Horizontal,
+    Vertical
+};
+
+struct Fold {
+    int value;
+    FoldType foldType;
+    Fold(int v, FoldType type) :
+        value(v),
+        foldType(type){}
+};
+
+template<typename T>
+void printMatrix(const std::vector<std::vector<T>>& matrix)
+{
+    size_t i = 0;
+    for(const auto& row : matrix) {
+        //std::cout << i << ": ";
+        for(const auto& item : row) {
+            std::cout << item;
+        }
+        std::cout << '\n';
+        i++;
+    }
+}
+
+void foldBottomHalfUp(std::vector<std::vector<char>>& board, const Fold& fold)
+{
+    // Use two pointer approach
+    // Here, we modify the row and iterate through each column
+    // Have upper and lower pointer
+    // upper = fold.value - 1
+    // lower = fold.value
+    // board[upper][i] = board[lower][i]
+    // upper--, lower++
+
+    int upper = fold.value - 1;
+    int lower = fold.value + 1;
+    // std::cout << '\n';
+
+    auto printRow = [](const std::vector<char>& row) {
+        for(auto c : row)
+            std::cout << c;
+        std::cout << '\n';
+    };
+
+    while(upper >= 0 && lower < board.size()) {
+        // std::cout << "Upper = " << upper << '\n';
+        // std::cout << "Lower = " << lower << '\n';
+        // std::cout << '\n';
+        // printRow(board[upper]);
+        // printRow(board[lower]);
+        // std::cout << '\n';
+        // std::cout << '\n';
+        // std::cout << '\n';
+
+        for(size_t i = 0; i < board[upper].size(); i++) {
+            char upperChar = board[upper][i];
+            char lowerChar = board[lower][i];
+
+            if(upperChar != lowerChar) {
+                board[upper][i] = '#';
+            }
+
+        }
+        upper--;
+        lower++;
+    }
+}
+
+void foldRightHalfLeft(std::vector<std::vector<char>>& board, const Fold& fold)
+{
+    int left = fold.value - 1;
+    int right = fold.value + 1;
+
+    while(left >= 0 && right < board[0].size()) {
+
+        std::cout << left << ' ' << right << '\n';
+
+        for(size_t i = 0; i < board.size(); i++) {
+            char upperChar = board[i][left];
+            char lowerChar = board[i][right];
+
+            if(upperChar != lowerChar) {
+                board[i][left] = '#';
+            }
+
+        }
+        left--;
+        right++;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    constexpr unsigned NumArgsExpected = 2;
+    if(argc != NumArgsExpected) {
+        std::cerr << "Usage: " << argv[0] << " <input-file>\n";
+        return -1;
+    }
+
+    std::ifstream infile(argv[1]);
+    std::string line;
+
+    typedef std::pair<int, int> Pair;
+    std::vector<Pair> orderedPairs;
+    std::vector<Fold> folds;
+
+    while(std::getline(infile, line)) {
+        
+        size_t commaIndex = line.find(',');
+        bool isOrderedPair = (commaIndex != std::string::npos);
+        bool isFold = (line.find("fold") != std::string::npos);
+
+        if(isOrderedPair) {
+            // Ordered pairs are always formatted as "x,y"
+            int x = std::stoi(line.substr(0, commaIndex));
+            int y = std::stoi(line.substr(commaIndex + 1));
+
+            orderedPairs.push_back({x, y});
+
+        } else if(isFold){
+            size_t equalsSignIndex = line.find("=");
+            int value = std::stoi(line.substr(equalsSignIndex + 1));
+
+            // Look for x and y in the string
+            // Fold lines are always formatted as
+            // fold [x|y]=[number]
+
+            size_t yIndex = line.find("y");
+            // A line with the equation y=constant is horizontal
+            bool isHorizontalLine = (yIndex != std::string::npos);
+
+            if(isHorizontalLine)
+                folds.push_back(Fold(value, FoldType::Horizontal));
+            else 
+                folds.push_back(Fold(value, FoldType::Vertical));
+        }
+
+    }
+
+    std::cout << "Number of dots: " << orderedPairs.size() << '\n';
+    
+    auto pairWithHighestXValue = std::max_element(
+        orderedPairs.begin(), 
+        orderedPairs.end(),
+        [](const auto& lhs, const auto& rhs) {
+            return lhs.first < rhs.first;
+        }
+    );
+
+    int highestXValue = pairWithHighestXValue->first;
+
+    auto pairWithHighestYValue = std::max_element(
+        orderedPairs.begin(), 
+        orderedPairs.end(),
+        [](const auto& lhs, const auto& rhs) {
+            return lhs.second < rhs.second;
+        }
+    );
+
+    int highestYValue = pairWithHighestYValue->second;
+    constexpr char Empty = '.';
+    constexpr char Dot = '#';
+
+    // Coordinates go from [0,0] to [maxX, maxY]
+    // This is a range of (maxX + 1, maxY + 1)
+    std::vector<std::vector<char>> board(
+        highestYValue + 1, 
+        std::vector<char>(highestXValue + 1, Empty));
+
+    for(const Pair& p : orderedPairs) {
+        board[p.second][p.first] = Dot;
+    }
+
+    //printMatrix(board);
+
+    // Folding algorithm
+    
+    Fold firstFold {folds[0]};
+    if(firstFold.foldType == FoldType::Horizontal) {
+        std::cout << "Folding along y=" << firstFold.value << '\n';
+        foldBottomHalfUp(board, firstFold);
+
+        for(size_t i = firstFold.value + 1; i < board.size(); i++) {
+            for(size_t j = 0; j < board[i].size(); j++) {
+                board[i][j] = '!';
+            }
+        }
+    }
+    else {
+        std::cout << "Folding along x=" << firstFold.value << '\n';
+        foldRightHalfLeft(board, firstFold);
+
+        for(size_t i = 0; i < board.size(); i++) {
+            for(size_t j = firstFold.value + 1; j < board[i].size(); j++) {
+                board[i][j] = '!';
+            }
+        }
+    }
+
+    unsigned numDots = 0;
+    
+    
+    //printMatrix(board);
+
+    for(const auto& row : board) {
+        for(const auto& c : row) {
+            if(c == Dot) {
+                numDots++;
+            }
+        }
+    }
+    std::cout << "Number of dots after first fold: " << numDots << '\n';
+
+    return 0;
+}
