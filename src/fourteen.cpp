c2271e4dcb4b96c1f7b58bfdd3c0438e148f7b38
Connor D <connordecamp@gmail.com>
2022-09-15 12:28:07 -0500

    Solve day fourteen part two, except code gives off by one answer

--- src/fourteen.cpp
+++ src/fourteen.cpp
@@ -13,6 +13,107 @@ std::unordered_map<char, uint64_t> getLetterFrequencies(const std::string& s)
     return frequencyMap;
 }
 
+uint64_t partTwo(const char* filename)
+{
+    std::ifstream infile(filename);
+    std::string line;
+    std::unordered_map<std::string, std::pair<std::string, std::string>> insertionRules;
+    std::unordered_map<std::string, uint64_t> polymer;
+
+    const std::string Arrow {" -> "};
+    char firstChar = '-';
+
+    while(std::getline(infile, line)) {
+
+        if(line.size() == 0)
+            continue;
+
+        size_t arrowIndex = line.find(Arrow);
+        bool isInsertionRule {arrowIndex != std::string::npos};
+
+        if(isInsertionRule) {
+            std::string key {line.substr(0, arrowIndex)};
+            char value = line.substr(arrowIndex + Arrow.length())[0];
+
+            std::string opt1 {{key[0], value}};
+            std::string opt2 {{value, key[1]}};
+
+            insertionRules.insert({key, {opt1, opt2}});
+        }
+        else {
+            
+            // Create polymer
+            firstChar = line[0];
+            constexpr unsigned WindowSize {2};
+            for(size_t i = 0; i <= line.size() - WindowSize; i++) {
+                std::string key {line.substr(i, WindowSize)};
+                polymer[key]++;
+            }
+        }
+    }
+
+    constexpr unsigned Steps = 40;
+
+    for(size_t i = 0; i < Steps; i++) {
+        std::unordered_map<std::string, uint64_t> newPolymer;
+
+        for(const auto& iter : polymer) {
+            std::string key = iter.first;
+            
+            bool isInsertionRule = (insertionRules.find(key) != insertionRules.end());
+            uint64_t amountInPolymer = polymer[key];
+            
+            if(isInsertionRule) {
+                std::pair<std::string, std::string> newStrings {
+                    insertionRules.find(key)->second};
+
+                newPolymer[newStrings.first] += amountInPolymer;
+                newPolymer[newStrings.second] += amountInPolymer;
+            }
+            else {
+                newPolymer[key] = amountInPolymer;
+            }
+        }
+
+        polymer = std::move(newPolymer);
+    }
+
+    std::unordered_map<char, uint64_t> frequency;
+    for(const auto& pair : polymer) {
+        frequency[pair.first[0]] += pair.second;
+        frequency[pair.first[1]] += pair.second;
+    }
+
+
+    for(auto& iter : frequency) {
+        iter.second /= 2;
+    }
+
+
+    for(auto& iter : frequency) {
+        std::cout << iter.first << ' ' << iter.second << '\n';
+    }
+
+    typedef std::pair<char, uint64_t> pair_type;
+    auto comparator = [](const pair_type& p1, const pair_type& p2){
+        return p1.second < p2.second;
+    };
+
+    uint64_t highestFrequency = std::max_element(
+        frequency.begin(),
+        frequency.end(),
+        comparator
+    )->second;
+
+    uint64_t lowestFrequency = std::min_element(
+        frequency.begin(),
+        frequency.end(),
+        comparator
+    )->second;
+
+    return (highestFrequency - lowestFrequency);
+}
+
 int main(int argc, char** argv)
 {
     constexpr unsigned NumExpectedArgs = 2;
@@ -21,6 +122,11 @@ int main(int argc, char** argv)
         return -1;
     }
 
+    std::cout << "Answer: " << partTwo(argv[1]) << '\n';
+
+    return 0;
+    // TODO
+    // Fix off by 1 error
     std::ifstream infile(argv[1]);
     std::string line;
 
